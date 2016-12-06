a8762c164a67ef590f0b98d13749f48460380226
Justin Zimmer <jtzimmer6@gmail.com>
2016-12-06 00:27:53 -0700

    svf

--- mix.exs
+++ mix.exs
@@ -15,12 +15,12 @@ defmodule NKSip.Mixfile do
   defp deps do
     [
       {:mix_erlang_tasks, "0.1.0"},
-      {:lager, "basho/lager.git", tag: "2.2.3", override: true},
+      {:lager, github: "basho/lager.git", tag: "2.2.3", override: true},
       {:jsx, github: "talentdeficit/jsx", tag: "v2.8.0", override: true},
       {:jiffy, github: "davisp/jiffy", tag: "0.14.8", override: true},
       {:eper, github: "massemanet/eper", tag: "0.97.6", override: true},
       {:meck, github: "basho/meck", tag: "0.8.2", override: true},
-      {:nklib, "phatjmo/nklib", override: true},
+      {:nklib, github: "phatjmo/nklib", override: true},
       {:ranch, github: "ninenines/ranch",  tag: "1.1.0", override: true},
       {:cowlib, github: "ninenines/cowlib", tag: "1.3.0", override: true},
       {:cowboy, github: "extend/cowboy", branch: "master", override: true},
